faf9dbd230db21e1cca418480a638079189f7ab3
O-Zone-Guy <alkershomar@gmail.com>
2020-06-23 21:26:18 -0400

    feat - Created Paddle.h

---
+++ src/headers/Paddle.h
@@ -0,0 +1,27 @@
+#ifndef PADDLE
+#define PADDLE
+
+#include "Point2D.h"
+#include <curses.h>
+
+const double SPEED = 10;
+
+enum Direction { Up, Down };
+
+class Paddle {
+  int len;
+  Point2D pos;
+  WINDOW win;
+
+public:
+  Paddle(Point2D p, WINDOW w, int l): pos(p), win(w), len(l){};
+  void move(Direction dir);
+  void move(float time);
+  void moveTo(Point2D newPos);
+  Point2D getPos(){ return pos;}
+  Point2D getTop();
+  Point2D getBot();
+};
+
+
+#endif
