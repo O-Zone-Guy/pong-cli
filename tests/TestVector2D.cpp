3972b3cb89f1ca6b9e87f06361a0484791dd0565
O-Zone-Guy <alkershomar@gmail.com>
2020-07-06 00:34:54 -0400

    test - Renamed variables and added objects for mag_rad tests

--- tests/TestVector2D.cpp
+++ tests/TestVector2D.cpp
@@ -1,28 +1,35 @@
 #include "Vector2D.h"
 #include "gtest/gtest.h"
+#include <cmath>
 
 class TestVector2D : public ::testing::Test {
 protected:
-  Vector2D vR0 {0, 0};
-  Vector2D vR1 {10, 10};
-  Vector2D vR2 {10, 0};
-  Vector2D vR3 {0, 10};
-  Vector2D vR4 {10, -10};
+  Vector2D cR0 {0, 0};
+  Vector2D cR1 {10, 10};
+  Vector2D cR2 {10, 0};
+  Vector2D cR3 {0, 10};
+  Vector2D cR4 {10, -10};
+
+  Vector2D pR0 {0.0, 0.0};
+  Vector2D pR1 {5.0, 1.0};
+  Vector2D pR2 {-3.5, M_PI/2};
+  Vector2D pR3 {10.0, 3*M_PI};
+  Vector2D pR4 {3.4, -5.5*M_PI};
 };
 
 TEST_F(TestVector2D, getter_X_Y){
-  ASSERT_NEAR(vR0.getX(), 0, 1E-12);
-  ASSERT_NEAR(vR0.getY(), 0, 1E-12);
+  EXPECT_EQ(cR0.getX(), 0);
+  EXPECT_EQ(cR0.getY(), 0);
 
-  ASSERT_NEAR(vR1.getX(), 10, 1E-12);
-  ASSERT_NEAR(vR1.getY(), 10, 1E-12);
+  EXPECT_EQ(cR1.getX(), 10);
+  EXPECT_EQ(cR1.getY(), 10);
 
-  ASSERT_NEAR(vR2.getX(), 10, 1E-12);
-  ASSERT_NEAR(vR2.getY(), 0, 1E-12);
+  EXPECT_EQ(cR2.getX(), 10);
+  EXPECT_EQ(cR2.getY(), 0);
 
-  ASSERT_NEAR(vR3.getX(), 0, 1E-12);
-  ASSERT_NEAR(vR3.getY(), 10, 1E-12);
+  EXPECT_EQ(cR3.getX(), 0);
+  EXPECT_EQ(cR3.getY(), 10);
 
-  ASSERT_NEAR(vR4.getX(), 10, 1E-12);
-  ASSERT_NEAR(vR4.getY(), -10, 1E-12);
+  EXPECT_EQ(cR4.getX(), 10);
+  EXPECT_EQ(cR4.getY(), -10);
 }
